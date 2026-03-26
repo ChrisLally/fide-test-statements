33f3a1e02ae43da024d40b8259de3b4ac38717ab
Chris Lally <24978693+ChrisLally@users.noreply.github.com>
2026-03-26 12:06:02 -0400

    flatening cli more

--- .fide/queries/fide-graph-pg-local/recentStatements.sql
+++ .fide/queries/fide-graph-pg-local/recentStatements.sql
@@ -1,1 +1,3 @@
+-- description: Test
+
 select * from fide_graph.statements order by created_at desc limit 10
--- .fide/queries/sqlite/recentStatements.sql
+++
@@ -1,4 +0,0 @@
--- statement-store: sqlite
--- description: Recent statements for quick inspection
-
-select * from statements limit 10
