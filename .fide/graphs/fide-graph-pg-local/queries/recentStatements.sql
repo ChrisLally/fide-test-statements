bfd76d3583a1ec4f7d31c3a23e6bac8c4a434087
Chris Lally <24978693+ChrisLally@users.noreply.github.com>
2026-03-27 07:49:56 -0400

    b

--- .fide/graphs/fide-graph-pg-local/queries/recentStatements.sql
+++ .fide/graphs/fide-graph-pg-local/queries/recentStatements.sql
@@ -1,3 +1,3 @@
--- description: Test
+-- description: Test!
 
 select * from fide_graph.statements order by created_at desc limit 10
--- .fide/queries/fide-graph-pg-local/recentStatements.sql
+++
@@ -1,3 +0,0 @@
--- description: Test
-
-select * from fide_graph.statements order by created_at desc limit 10
